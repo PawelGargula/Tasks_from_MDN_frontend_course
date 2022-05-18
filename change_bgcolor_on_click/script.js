c1e35b6390ba1d34cff5912b59039f3f406f0357
Paweł Gargula <pawel.gargula@interia.pl>
2022-05-18 18:54:28 +0200

    Add handler, which change bgcolor of buttonBar on appropriate button click

--- change_bgcolor_on_click/script.js
+++ change_bgcolor_on_click/script.js
@@ -1,1 +1,8 @@
 const buttonBar = document.querySelector('.button-bar');
+
+buttonBar.addEventListener('click', e => {
+    const choosedColor = e.target.getAttribute('data-color');
+    if (choosedColor) {
+        e.currentTarget.style.backgroundColor = choosedColor;
+    }
+});
