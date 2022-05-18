1dfe61ab638f2d76d55d1ebc99c7457a0b48e58a
Paweł Gargula <pawel.gargula@interia.pl>
2022-05-18 20:02:58 +0200

    Add array with file names. Add event handler for images. Add event handler for button.

--- image_gallery/main.js
+++ image_gallery/main.js
@@ -5,11 +5,32 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const fileNames = [ 
+    'pic1.jpg',
+    'pic2.jpg',
+    'pic3.jpg',
+    'pic4.jpg',
+    'pic5.jpg', 
+];
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+fileNames.forEach(fileName => {
+    const newImg = new Image();
+    newImg.src = `images/${fileName}`;
+    thumbBar.appendChild(newImg);
+    newImg.addEventListener('click', e => displayedImage.src = e.target.src);
+});
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', e => {
+    let currentClass = e.target.getAttribute('class');
+    if (currentClass === 'dark') {
+        e.target.setAttribute('class', 'light');
+        e.target.textContent = 'Lighten';
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    } else {
+        e.target.setAttribute('class', 'dark');
+        e.target.textContent = 'Darken';
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+})
