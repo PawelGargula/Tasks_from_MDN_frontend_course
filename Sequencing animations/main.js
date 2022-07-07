0d2835de6136fd40855f7b3139508c0ca719015f
Paweł Gargula <pawel.gargula@interia.pl>
2022-07-07 19:59:58 +0200

    Add solutions

--- Sequencing animations/main.js
+++ Sequencing animations/main.js
@@ -12,3 +12,35 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+//#region Callback hell
+// alice1.animate(aliceTumbling, aliceTiming).finished.then(
+//   () => alice2.animate(aliceTumbling, aliceTiming).finished.then(
+//     () => alice3.animate(aliceTumbling, aliceTiming)
+//   )
+// );
+//#endregion
+
+//#region  then catch
+// alice1.animate(aliceTumbling, aliceTiming).finished.then(
+//   () => alice2.animate(aliceTumbling, aliceTiming).finished
+// ).then(
+//   () => alice3.animate(aliceTumbling, aliceTiming)
+// ).catch(
+//   e => console.error(`Detect error: '${e}', while animating.`)
+// );
+//#endregion
+
+//#region async await
+async function animateOneByOne() {
+  try {
+    await alice1.animate(aliceTumbling, aliceTiming).finished;
+    await alice2.animate(aliceTumbling, aliceTiming).finished;
+    alice3.animate(aliceTumbling, aliceTiming);
+  } catch (e) {
+    console.error(`Detect error: '${e}', while animating.`)
+  }
+}
+
+animateOneByOne();
+//#endregion
